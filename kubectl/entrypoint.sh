222a848fe1e3b05f2c37973c3840751d468566b8
Wouter de Vos <wouter@codaisseur.com>
2019-09-26 23:00:35 +0200

    Downgrade base container and update entrypoint

--- kubectl/entrypoint.sh
+++ kubectl/entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 set -e
 
+gcloud config set project "$GCLOUD_PROJECT"
+gcloud config set container/use_client_certificate True
+gcloud container clusters get-credentials "$K8S_CLUSTER_NAME" --zone "$GCLOUD_ZONE"
+
 sh -c "kubectl $*"
