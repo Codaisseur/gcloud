0abed4f1aaa3009afda5b6f3dc96e8b134d3dc09
Wouter de Vos <wouter@codaisseur.com>
2019-09-26 23:08:23 +0200

    Standardize env var names

--- kubectl/entrypoint.sh
+++ kubectl/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-gcloud config set project "$GCLOUD_PROJECT"
+gcloud config set project "$CLOUDSDK_CORE_PROJECT"
 gcloud config set container/use_client_certificate True
-gcloud container clusters get-credentials "$K8S_CLUSTER_NAME" --zone "$GCLOUD_ZONE"
+gcloud container clusters get-credentials "$CLOUDSDK_CONTAINER_CLUSTER" --zone "$CLOUDSDK_COMPUTE_ZONE"
 
 sh -c "kubectl $*"
