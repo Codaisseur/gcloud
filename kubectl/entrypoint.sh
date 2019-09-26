79ff34a8341b2a7c8f110f8ac4acf76a62d8116e
Wouter de Vos <wouter@codaisseur.com>
2019-09-27 00:00:42 +0200

    Try to fix commands

--- kubectl/entrypoint.sh
+++ kubectl/entrypoint.sh
@@ -4,9 +4,7 @@ set -e
 export KUBECONFIG=${GITHUB_WORKSPACE}/.kube/config
 mkdir -p $(dirname $KUBECONFIG)
 
-gcloud config set project "$CLOUDSDK_CORE_PROJECT"
-gcloud config set container/use_client_certificate True
-gcloud services enable container.googleapis.com
-gcloud container clusters get-credentials "$CLOUDSDK_CONTAINER_CLUSTER" --zone "$CLOUDSDK_COMPUTE_ZONE"
+sh -c "gcloud config set project ${CLOUDSDK_CORE_PROJECT}"
+sh -c "gcloud container clusters get-credentials ${CLOUDSDK_CONTAINER_CLUSTER} --zone ${CLOUDSDK_COMPUTE_ZONE}"
 
 sh -c "kubectl $*"
